36c45d6e1f5a97d12276edfcaef2b32843ad88cf
Max Blancarte <blancarte@gmail.com>
2025-07-06 10:46:15 -0700

    chore: refactor client rpc

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,28 +1,43 @@
-import { createServerFn } from "@tanstack/react-start";
+import { createIsomorphicFn } from "@tanstack/react-start";
 import { getWebRequest } from "@tanstack/react-start/server";
 import { hc } from "hono/client";
-// import { clientEnv } from "@/env";
 import type { AppType } from "@/server";
 
-export const getRequestHeaders = createServerFn({ method: "GET" }).handler(
-  async () => {
-    const request = getWebRequest();
-    const headers = new Headers(request?.headers);
+const apiURL = import.meta.env.VITE_BETTER_AUTH_URL;
 
-    return Object.fromEntries(headers);
-  },
-);
+const serverFetcher: typeof fetch = async (input, init) => {
+  const request = getWebRequest();
+  const headers = new Headers(request?.headers);
 
-export const client = hc<AppType>(import.meta.env.VITE_BETTER_AUTH_URL, {
-  fetch: (async (input, init) => {
-    const heeaderObj = await getRequestHeaders();
+  return fetch(input, {
+    ...init,
+    credentials: "include",
+    headers: {
+      Cookie: headers.get("cookie") || "",
+      ...Object.fromEntries(headers),
+      ...init?.headers,
+    },
+  });
+};
 
-    return fetch(input, {
-      ...init,
-      credentials: "include",
-      headers: {
-        Cookie: heeaderObj.cookie,
-      },
-    });
-  }) satisfies typeof fetch,
-});
+const clientFetcher: typeof fetch = async (input, init) => {
+  return fetch(input, {
+    ...init,
+    credentials: "include",
+  });
+};
+
+// Create isomorphic client using createIsomorphicFn pattern
+const getClient = createIsomorphicFn()
+  .client(() =>
+    hc<AppType>(apiURL, {
+      fetch: clientFetcher,
+    }),
+  )
+  .server(() =>
+    hc<AppType>(apiURL, {
+      fetch: serverFetcher,
+    }),
+  );
+
+export const client = getClient();
